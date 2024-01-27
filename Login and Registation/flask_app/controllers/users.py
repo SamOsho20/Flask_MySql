40304eb80c535121bc0f17cf60f35ce7e3416715
Sam_Osho <Sosho108@marian.edu>
2024-01-27 10:03:16 -0500

    Further updates!

--- Login and Registation/flask_app/controllers/users.py
+++ Login and Registation/flask_app/controllers/users.py
@@ -1,3 +1,58 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
-from flask_app.models.user  import user
+from flask_app.models.user  import User
+from flask_bcrypt import Bcrypt        
+bcrypt = Bcrypt(app)
+
+
+@app.route('/')
+def show_loginandreg():
+    return render_template('index.html')
+
+
+@app.route('/register_user', methods = ['POST'])
+def process_reg():
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    if User.validate_registration(request.form):
+        data = {
+            'first_name' : request.form['first_name'],
+            'last_name' : request.form['last_name'],
+            'email' : request.form['email'],
+            'password': pw_hash
+            #confrim password should not be added as column as database
+            }
+        User.create_user(data)
+        #save the data t a method
+        user_id = User.create_user(data)
+
+        session['user_id'] = user_id
+        #we are bassically saying whoever is logged in, 
+        # that is  the id of the persons info that will be displayed on 
+        #see  dashboard route to see how user_id gets forwarded
+
+        return redirect (f"/dashboard/{session['user_id']}")
+    if not pw_hash:
+        return redirect ('/')
+
+    return redirect('/')
+
+#is this app route  we will display the  user ins sessions name with a oppurtunity to also logout 
+
+@app.route('/dashboard/<int:user_id>')
+def show_user(user_id):
+    if session['user_id']:
+        current_user = User.get_one_user({'user_id': session['user_id']})
+        return render_template ('dashboard.html', current_user = current_user)
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect ('/')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    return redirect ('/')
+
+    #  'bool' object is not subscriptable error 
+    # return cls(results[0])
--- Login and Registation/flask_app/models/user.py
+++ Login and Registation/flask_app/models/user.py
@@ -1,7 +1,83 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 class User:
     DB = 'loginandreg_schema'
     def __init__(self,data):
-        self.name = data['name']
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @classmethod
+    def create_user(cls,data):
+        query = """INSERT INTO USER
+                    (first_name,last_name,email,password,created_at,updated_at) 
+                    VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s,NOW(),NOW());
+                    """
+        results = connectToMySQL(cls.DB).query_db(query,data)
+        return results
+    
+    @classmethod
+    def get_one_user(cls,user_id):
+        query = """SELECT * FROM USER
+                    Where id = %(user_id)s;
+                    """
+                    #make sure names match up for id like above on lines 29,31, "user_id"
+        results = connectToMySQL(cls.DB).query_db(query,user_id)
+        return cls(results[0])
+    
+
+    #  'bool' object is not subscriptable error 
+    # return cls(results[0])
+    # another name for iterating is subscripting !!
+
+
+    @staticmethod
+    def validate_registration(request):
+        is_valid = True
+        if not request['email']:
+            is_valid = False
+            flash('Enter a Email', 'reg_flash')
+
+        elif not EMAIL_REGEX.match(request["email"]):
+            is_valid = False
+            flash('invalid email address', 'reg_flash')
+
+        if not request['first_name']:
+            is_valid = False
+            flash('Enter a First Name', 'reg_flash')
+
+        elif len(request["first_name"])< 2:
+            is_valid = False
+            flash('First Name must be at least 2 characters', 'reg_flash')
+
+        if not request['last_name']:
+            is_valid = False
+            flash('Enter a Last Name', 'reg_flash')
+
+        elif len(request["last_name"])< 2:
+            is_valid = False
+            flash('Last Name must be at least 2 characters', 'reg_flash')
+
+        if not request['password']:
+            is_valid = False
+            flash('Enter A password', 'reg_flash')
+
+        elif len(request["password"])< 8:
+            is_valid = False
+            flash('Password Must Be over 8 characters', 'reg_flash')
+        
+        if request['password'] != request['confirm_password']:
+            is_valid = False
+            flash('Passwords Dont Match', 'reg_flash')
+
+        return is_valid
+        #must have return is_valid!!!!!!!
+    
